3cd1a5e6997fae244704fae1df41d42f528b3713
EnergieNVidia77 <toufictalha@gmail.com>
2023-01-16 16:57:41 +0100

    Something might might not :)

--- tps/miner.py
+++ tps/miner.py
@@ -1,12 +1,16 @@
 import threading
 import sys
 import socket
+import uuid
 
 sock_recv_conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock_recv_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 sock_emit_conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 sock_emit_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+my_uuid = uuid.uuid4()
+print(my_uuid)
 
 my_ip = sys.argv[1]
 my_port = sys.argv[2]
@@ -16,23 +20,43 @@ sock_recv_conn.bind(my_addr)
 sock_recv_conn.listen()
 
 users_list = {}
+users_thread = {}
 
 try:
     target_ip = sys.argv[3]
     target_port = sys.argv[4]
-    print("Miner B")
 except IndexError:
     print("Miner A")
 
+def listenMsg(id):
+    while(True):
+        data = users_list[id].recv(1024).decode()
+        data = data.split()
+        if not data[0] in users_list:
+            users_list[data[0]] = data[1]
+            print(users_list)
 
+def addTolist(id, conn):
+    users_list[id] = conn
+    t = threading.Thread(target= listenMsg)
+    users_thread[id] = t
 
 def connect():
     while True:
         conn, addr = sock_recv_conn.accept()
         data = conn.recv(1024).decode()
+        addTolist(data, conn)
         print(data)
-        data_to_send = "You have successfully connected to your target"
+        print(users_list)
+        data_to_send = str(my_uuid)
         conn.send(data_to_send.encode())
+        for user in users_list:
+            print(user)
+            data = str(conn)
+            users_list[user].send(data.encode())
+            str_user = str(users_list[user])
+            conn.send(str_user.encode())
+        print(users_list)
 
 
 def main():
@@ -49,10 +73,14 @@ def main():
 
         target_addr = (target_ip, int(target_port))
         sock_emit_conn.connect(target_addr)
-        data = "Miner B has connected"
+        data = str(my_uuid)
         sock_emit_conn.send(data.encode())
         data_recv = sock_emit_conn.recv(1024).decode()
+        addTolist(data_recv, sock_emit_conn)
         print(data_recv)
+        #print(users_list)
+
+
 
 
     except NameError:
