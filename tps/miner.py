cd69f07e506f117a03db1d05d257afa13aca25b4
EnergieNVidia77 <toufictalha@gmail.com>
2023-01-16 15:25:52 +0100

    Foundation

--- tps/miner.py
+++ tps/miner.py
@@ -2,16 +2,23 @@ import threading
 import sys
 import socket
 
-sock_accept = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-sock_accept.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock_recv_conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+sock_recv_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock_emit_conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+sock_emit_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 my_ip = sys.argv[1]
 my_port = sys.argv[2]
 
+my_addr = (my_ip, int(my_port))
+sock_recv_conn.bind(my_addr)
+sock_recv_conn.listen()
+
 users_list = {}
 
 try:
-    target_ip_addr = sys.argv[3]
+    target_ip = sys.argv[3]
     target_port = sys.argv[4]
     print("Miner B")
 except IndexError:
@@ -21,22 +28,35 @@ except IndexError:
 
 def connect():
     while True:
-        conn, addr = sock_accept.accept()
+        conn, addr = sock_recv_conn.accept()
+        data = conn.recv(1024).decode()
+        print(data)
+        data_to_send = "You have successfully connected to your target"
+        conn.send(data_to_send.encode())
+
 
 def main():
 
-    connect_thread = threading.Thread(target=connect, daemon=True)
+    connection_thread = threading.Thread(target=connect)
+    connection_thread.start()
 
     print("My addr: " + my_ip)
     print("My port: " + my_port)
 
     try :
-        print("Target addr: " + target_ip_addr)
+        print("Target addr: " + target_ip)
         print("Target port: " + target_port)
+
+        target_addr = (target_ip, int(target_port))
+        sock_emit_conn.connect(target_addr)
+        data = "Miner B has connected"
+        sock_emit_conn.send(data.encode())
+        data_recv = sock_emit_conn.recv(1024).decode()
+        print(data_recv)
+
+
     except NameError:
         print("No target addr or target port defined")
-
-    return 0
 
 
 if __name__ == '__main__':
