0f32b02667470520e70a7c4e48434132f66efcdb
EnergieNVidia77 <toufictalha@gmail.com>
2023-01-17 16:55:17 +0100

    New achitecture and a better one

---
+++ tps/miner_tp/miner.py
@@ -0,0 +1,19 @@
+import sys
+import threading
+from miner_class import Miner
+
+my_ip = sys.argv[1]
+my_port = int(sys.argv[2])
+
+miner = Miner(my_ip, my_port)
+receive_thread = threading.Thread(target=miner.receive)
+receive_thread.start()
+
+try :
+    target_ip = sys.argv[3]
+    target_port = int(sys.argv[4])
+    miner.connect(target_ip, target_port)
+except IndexError:
+    print("No tarrget ip or port specified")
+
+
