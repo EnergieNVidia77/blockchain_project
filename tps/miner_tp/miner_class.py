97757ae689160559c36fcb6a91a250da92e35881
EnergieNVidia77 <toufictalha@gmail.com>
2023-01-19 18:39:09 +0100

    work??

--- tps/miner_tp/miner_class.py
+++ tps/miner_tp/miner_class.py
@@ -15,52 +15,52 @@ class Miner:
         self.uuid = str(uuid.uuid4())
         print(f"My uuid {self.uuid}")
         self.miners = {}
+        self.connected_miners = []
+        #self.miners[self.uuid] = port
 
-    def handle_client(self, conn, flag):
+    def handle_initial_exchange(self, conn, flag):
         while True:
-            recv_msg_encoded = conn.recv(1024)
-            if not recv_msg_encoded:
+            recv_msg = conn.recv(1024).decode()
+            if not recv_msg:
                 print("No data or connection lost")
                 break
-            recv_msg = pickle.loads(recv_msg_encoded)
-            if not recv_msg in self.miners.keys() and type(recv_msg) == str and flag:
-                self.miners[recv_msg] = conn
-                print(self.miners)
-                self.send_uuid(conn)
-            elif type(recv_msg) == str:
-                self.miners[recv_msg] = conn
+            array_recv_msg = recv_msg.split()
+            print(f"Miner's uuid {array_recv_msg[0]} and miner's listening port {array_recv_msg[1]}")
+            if not array_recv_msg[0] in self.miners:
+                self.miners[array_recv_msg[0]] = array_recv_msg[1]
                 print(self.miners)
             if flag:
+                self.send_info(conn)
+                time.sleep(0.5)
                 for miner in self.miners:
-                    #print(recv_msg[1])
-                    if miner != recv_msg and int(recv_msg[1]) < 9000:
-                        raddr = self.miners[miner].getpeername() 
-                        print(f"I sent this remote {raddr}")
-                        packed_raddr = pickle.dumps(raddr)
-                        conn.send(packed_raddr)
-            elif type(recv_msg) == tuple:
-                print(f"Remote I got {recv_msg}")
-                print("Trying to connect")
-                try:
-                    self.connect(recv_msg[0], recv_msg[1])
-                except ConnectionRefusedError:
-                    print("Connection refused")
+                    if miner != array_recv_msg[0]:
+                        msg_to_send = miner + " " + self.miners[miner]
+                        print(msg_to_send)
+                        conn.send(msg_to_send.encode())
+            else:
+                print(array_recv_msg[0])
+                print(array_recv_msg[1])
+                if not int(array_recv_msg[1]) in self.connected_miners:
+                    self.connect('127.0.0.1', int(array_recv_msg[1]))
 
     def receive(self):
         while True:
             conn, addr =  self.sock_recv_conn.accept()
             print(f"Connected with {addr}")
-            recv_thread = threading.Thread(target=self.handle_client, args=(conn, 1))
+            recv_thread = threading.Thread(target=self.handle_initial_exchange, args=(conn, 1))
             recv_thread.start()
     
-    def send_uuid(self, conn):
-        encoded_uuid = pickle.dumps(self.uuid)
-        conn.send(encoded_uuid)
+    def send_info(self, conn):
+        recv_addr = self.sock_recv_conn.getsockname()
+        all_info = self.uuid + " " + str(recv_addr[1])
+        encoded_info = all_info.encode()
+        conn.send(encoded_info)
 
     def connect(self, host, port):
         sock_emit_conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
         sock_emit_conn.connect((host, port))
         print(f"Connected to {port}")
-        self.send_uuid(sock_emit_conn)
-        recv_thread = threading.Thread(target=self.handle_client, args=(sock_emit_conn, 0))
+        self.connected_miners.append(port)
+        self.send_info(sock_emit_conn)
+        recv_thread = threading.Thread(target=self.handle_initial_exchange, args=(sock_emit_conn, 0))
         recv_thread.start()
