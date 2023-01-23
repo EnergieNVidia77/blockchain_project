b46661f41e5e1741d2189e9e9323809fda6c7737
Ladiscou <49318992+Ladiscou@users.noreply.github.com>
2023-01-23 19:39:25 +0100

    Structure d'arbre ok

---
+++ tps/miner_tp/merkel.py
@@ -0,0 +1,5 @@
+import merkelClass as mC
+
+L = [1]
+A = mC.makeMerkel(L)
+print(mC.printArbre(A))
---
+++ tps/miner_tp/merkelClass.py
@@ -0,0 +1,62 @@
+
+class merkelTree:
+    def __init__(self, v, nextL, nextR, nb = -1):
+        self.value = v
+        self.nextR = nextR
+        self.nextL = nextL
+        self.nb = nb
+    
+    def modifyValue(self, v):
+        self.value = v
+    
+    def evalValue(self):
+        if self.nextR == None and self.nextL == None:
+            return self.value
+        elif self.nextR == None and self.nextL != None:
+            return self.nextR.evalValue()
+        elif self.nextR != None and self.nextL == None:
+            return self.nextL.evalValue()
+        else :
+            return self.nextR.evalValue() + self.nextL.evalValue()
+    
+    def proof(self, i):
+        break
+                                                    
+
+
+
+    
+def findDepth(size):
+    pow2 = 0
+    div = size
+    while(div > 1):
+        pow2 += 1
+        div = div/2
+    return pow2
+    
+
+def printArbre(t):
+    if t == None :
+        return ""
+    else :
+        return f"{t.value}[{printArbre(t.nextL)}, {printArbre(t.nextR)}]"
+
+def makeMerkel(LV):
+    size = len(LV)
+    depth = findDepth(size)
+    if depth <= 0:
+        return merkelTree(LV[0], None, None, 0)    
+    else:
+        listR = []
+        listL = []
+        bound = 2**(depth-1)
+        for i in range(size):
+            if i < bound:
+                listL.append(LV[i])
+            else:
+                listR.append(LV[i])
+        toReturn = merkelTree(-1, makeMerkel(listL), makeMerkel(listR), size)
+        toReturnValue = toReturn.evalValue()
+        toReturn.modifyValue(toReturnValue)
+        return toReturn
+
